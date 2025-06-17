67f373d7c454276c01c5e2261272525c27d30c2b
PassusOh <87425805+ody0327@users.noreply.github.com>
2025-06-17 14:13:47 +0000

    third commit

---
+++ DataType/format07.py
@@ -0,0 +1,11 @@
+output_a = 52.0
+output_b = "{:g}".format(output_a) 
+print(output_a)
+print(output_b)
+
+a = input("> 1번째 숫자")
+b = input("> 2번째 숫자")
+print()
+
+print("{} + {} = {}".format(a, b, float(a) + float(b)))
+
