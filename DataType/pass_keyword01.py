2477b511dac083458c67f0a6584cdf12e98de8ee
PassusOh <87425805+ody0327@users.noreply.github.com>
2025-06-25 14:20:56 +0000

    fifth commit

---
+++ DataType/pass_keyword01.py
@@ -0,0 +1,10 @@
+number = input("정수 입력> ")
+number = int(number)
+
+if number > 0:
+
+    raise NotImplementedError("양수는 아직 구현되지 않았습니다.")
+else:
+
+    raise NotImplementedError("음수는 아직 구현되지 않았습니다.")
+
