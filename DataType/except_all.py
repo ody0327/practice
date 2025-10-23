b414f136dbdb491e7c1f50a74facac7f28c7446d
PassusOh <87425805+ody0327@users.noreply.github.com>
2025-10-23 13:57:42 +0000

    251023

---
+++ DataType/except_all.py
@@ -0,0 +1,18 @@
+list_number = [52, 273, 32, 103, 90, 10, 275]
+
+try:
+    number_input = int(input("정수 입력> "))
+    print("{}번째 요소: {}".format(number_input, list_number[number_input]))
+    예외.발생()
+except ValueError as exception:
+    #ValueError가 발생하는 경우
+    print("정수를 입력해라")
+    print("exception:", exception)
+except IndexError as exception:
+    #IndexError가 발생하는 경우
+    print("리스트의 인덱스 벗어남")
+    print("exception:", exception)
+except Exception as exception:
+    #모든 예외가 발생하는 경우
+    print("파악하지 못한 예외가 발생했음")
+    print(type(exception), exception)
---
+++ DataType/except_as.py
@@ -0,0 +1,13 @@
+list_number = [52, 273, 32, 103, 90, 10, 275]
+
+try:
+    number_input = int(input("정수 입력> "))
+    print("{}번째 요소: {}".format(number_input, list_number[number_input]))
+except ValueError as exception:
+    #ValueError가 발생하는 경우
+    print("정수를 입력해라")
+    print("exception:", exception)
+except IndexError as exception:
+    #IndexError가 발생하는 경우
+    print("리스트의 인덱스 벗어남")
+    print("exception:", exception)
